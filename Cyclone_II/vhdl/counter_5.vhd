3dcc9d2c41f933b88a4a91cf2fb51c19c7b05721
Katrin  Baechli <baek@zhaw.ch>
2015-10-07 18:16:30 +0200

    Full Version of Glitches. Code not cleand yet.

--- Cyclone_II/vhdl/counter_5.vhd
+++ Cyclone_II/vhdl/counter_5.vhd
@@ -7,13 +7,9 @@
 -- Date     |Name      |Modification
 ------------|----------|-------------------------------------------------------
 -- 05.10.15	| baek     | init
+-- 06.10.15 | baek     | add cnt-singal and clock
+-------------------------------------------------------------------------------
 
--------------------------------------------------------------------------------
--- To Do:
--- Change 8 std_logic IO to std_logic_vector
--- Change cnt type from int -> std_logic_vector
--- Deled typcaste bevor q 
--------------------------------------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -22,24 +18,27 @@ use ieee.numeric_std.all;
 
 entity counter_5 is
 	port(	clk: 				in std_logic;
-			counter_reset:	out std_logic; -- to detect glitches
+			-- To detect glitches
+			counter_reset:	out std_logic; 
 			-- Routing Ports
 			q_0_out:			out std_logic;
 			q_1_out:			out std_logic;
 			q_2_out:			out std_logic;
 			q_3_out:			out std_logic;
-			q_4_out:			out std_logic;
-			q_5_out:			out std_logic;
-			q_6_out:			out std_logic;
-			q_7_out:			out std_logic;			
+			------
+			--q_4_out:			out std_logic;
+			--q_5_out:			out std_logic;
+			--q_6_out:			out std_logic;
+			q_7_out:			out std_logic;	
+			
 			q_0_in:			in  std_logic;
 			q_1_in:			in  std_logic;
 			q_2_in:			in  std_logic;
-			q_3_in:			in  std_logic;
-			q_4_in:			in  std_logic;
-			q_5_in:			in  std_logic;
-			q_6_in:			in  std_logic;
-			q_7_in:			in  std_logic
+			q_3_in:			in  std_logic
+			--q_4_in:			in  std_logic;
+			--q_5_in:			in  std_logic;
+			--q_6_in:			in  std_logic;
+			--q_7_in:			in  std_logic
 	);
 end entity;
 
@@ -49,8 +48,6 @@ end entity;
 ----------------------------------------------------------------------------------
 architecture rtl of counter_5 is 
 
-constant cnt_max:    std_logic_vector(7 downto 0):= "00000111";  -- 159 " 10011111"
-
 signal  cnt: 			integer range 0 to 15 	:= 0;
 signal  next_cnt: 	integer range 0 to 15 	:= 0;
 signal  reset_cnt: 	std_logic 					:= '0';  -- asynchronous
@@ -58,7 +55,8 @@ signal  q:           std_logic_vector(7 downto 0) := "00000000";
 signal  q_z:         std_logic_vector(7 downto 0) := "00000000";
 signal  delay:			std_logic					:= '0';
 signal  zero:			std_logic					:= '0';
-signal  zero_next:			std_logic					:= '0';
+signal  zero_next:	std_logic					:= '0';  -- regular counter signal
+signal  sig_clk:		std_logic					:= '0';
 
 begin
 
@@ -67,70 +65,77 @@ begin
 	begin			
 		if (rising_edge(clk)) then	
 				cnt <= next_cnt;
-			zero <= zero_next;	
+				zero <= zero_next;	
+				sig_clk <= not sig_clk; 
 		end if;
 	end process;
 
-		-- type conversion
-		q <= std_logic_vector(to_unsigned(cnt, 8));
+	-- type conversion
+   q <= std_logic_vector(to_unsigned(cnt, 8));
 
-		  q_0_out  <=  q(0);   
-		  q_1_out  <=  q(1);
-		  q_2_out  <=  q(2);
-		  q_3_out  <=  q(3);     
-		  q_z(4)   <=  q(4);     -- as quick as possible (no routing)
-		  q_5_out  <=  q(5);
-		  q_6_out  <=  q(6);   
-		  --q_7_out  <=  q(7);
-		  -- Read Signal q_z from GPIO Ports
-		  q_z(0)	  <=  q_0_in;   
-		  q_z(1)	  <=  q_1_in;
-		  q_z(2)	  <=  q_2_in;
-		  --q_z(3)	  <=  q_3_in;  -- delay (-> logic path in process)
-		  --q_z(4)	  <=  q_4_in;  -- no routing (direct linking)
-		  q_z(5)	  <=  q_5_in;
-		  q_z(6)	  <=  q_6_in;  
-		  q_z(7)	  <=  q_7_in;
+   q_0_out  <=  q(0);   -- 4 FF
+   q_1_out  <=  q(1);
+   q_2_out  <=  q(2);
+   q_3_out  <=  q(3);  
+   ------------------
+   --q_z(4)   <=  q(4);    -- not used 
+   --q_5_out  <=  q(5);
+   --q_6_out  <=  q(6);   
+   --q_7_out  <=  q(7);
+  
+   -- Read Signal q_z from GPIO Ports
+   q_z(0)	  <=  q_0_in;   -- 4 FF
+   q_z(1)	  <=  q_1_in;
+   q_z(2)	  <=  q_2_in;
+   --q_z(3)	  <=  q_3_in; 
+   ----------------------
+   --q_z(4)	  <=  q_4_in;  -- not used
+   --q_z(5)	  <=  q_5_in;
+   --q_z(6)	  <=  q_6_in;  
+   --q_z(7)	  <=  q_7_in;
 	
-	-- input logic process ----------------------------
+	
+------------------------------------------------------
+-- input
+------------------------------------------------------	
+		
+	-- counter
 	count_up: process(ALL)	
 	begin	
 		-- asynchronous
 		next_cnt <= cnt + 1;
 	end process;
-	
-	-- output logic process ---------------------------
-	output: process(ALL)	
-	begin	
-	   -- asynchronous
-		if q_z(0) = '1' AND q_z(1) = '1' AND q(2) = '1' AND q(3) = '1'
-		THEN reset_cnt <= '1';
-		ELSE reset_cnt <= '0';
-		END IF;
-		
-		
---		if (q_z = cnt_max) then				
---				reset_cnt <= '1';
---		else 				
---				reset_cnt <= '0';
---		end if;		
-	end process;
-	
-		check: process(ALL)	
-	begin	
-	   -- asynchronous
-		if cnt = 15
-		THEN zero_next <= '1';
-		ELSE zero_next <= '0';
-		END IF;
-		
 
---		end if;		
+	
+	
+------------------------------------------------------
+-- output
+------------------------------------------------------	
+	
+	set_glitch: process(ALL)	
+	begin	
+	   -- asynchronous: value 15
+		if ( q_z(0) = '1' AND q_z(1) = '1' AND q(2) = '1' AND q(3) = '1') then
+				reset_cnt <= '1';
+		else 
+				reset_cnt <= '0';
+		end if;	
 	end process;
 
+
+	
+	set_cnt: process(ALL)	
+	begin	
+	   -- asynchronous
+		if cnt = 15 then
+			zero_next <= '1';
+		else 
+			zero_next <= '0';
+		end if;	
+	end process;
+
+	--q_4_out <= sig_clk;
 	q_7_out <= zero;
-	
-	-- Concourent assignments ---------------------------
 	counter_reset <= reset_cnt;
 
 end rtl;
--- Cyclone_II/vhdl/top_counter_verification_5.vhd
+++ Cyclone_II/vhdl/top_counter_verification_5.vhd
@@ -23,19 +23,20 @@ ENTITY top_counter_verification_5 IS
 					GPIO_1_2:			OUT std_logic;
 					GPIO_1_4:			OUT std_logic;
 					GPIO_1_6:			OUT std_logic;
-					GPIO_1_8:			OUT std_logic;
-					GPIO_1_10:			OUT std_logic;
-					GPIO_1_12:			OUT std_logic;
-					GPIO_1_14:			OUT std_logic;
+					-----------------
+					GPIO_1_8:			OUT std_logic;     -- clk
+					--GPIO_1_10:			OUT std_logic;
+					--GPIO_1_12:			OUT std_logic;
+					GPIO_1_14:			OUT std_logic;     -- zero
 				
 					GPIO_1_1:			IN std_logic;
 					GPIO_1_3:			IN std_logic;
 					GPIO_1_5:			IN std_logic;
-					GPIO_1_7:			IN std_logic;
-					GPIO_1_9:			IN std_logic;
-					GPIO_1_11:			IN std_logic;
-					GPIO_1_13:			IN std_logic;
-					GPIO_1_15:			IN std_logic
+					GPIO_1_7:			IN std_logic
+					--GPIO_1_9:			IN std_logic;
+					--GPIO_1_11:			IN std_logic;
+					--GPIO_1_13:			IN std_logic;
+					--GPIO_1_15:			IN std_logic
 		);
 END top_counter_verification_5;
 
@@ -55,19 +56,20 @@ ARCHITECTURE blocks OF top_counter_verification_5 IS
 							q_1_out:			out std_logic;
 							q_2_out:			out std_logic;
 							q_3_out:			out std_logic;
-							q_4_out:			out std_logic;
-							q_5_out:			out std_logic;
-							q_6_out:			out std_logic;
-							q_7_out:			out std_logic;		
+							
+							--q_4_out:			out std_logic; -- clk
+							--q_5_out:			out std_logic;
+							--q_6_out:			out std_logic;
+							q_7_out:			out std_logic;		-- zero
 							
 							q_0_in:			in  std_logic;
 							q_1_in:			in  std_logic;
 							q_2_in:			in  std_logic;
-							q_3_in:			in  std_logic;
-							q_4_in:			in  std_logic;
-							q_5_in:			in  std_logic;
-							q_6_in:			in  std_logic;
-							q_7_in:			in  std_logic
+							q_3_in:			in  std_logic
+							--q_4_in:			in  std_logic;
+							--q_5_in:			in  std_logic;
+							--q_6_in:			in  std_logic;
+							--q_7_in:			in  std_logic
 					);
 		END COMPONENT; 
 		
@@ -85,19 +87,20 @@ ARCHITECTURE blocks OF top_counter_verification_5 IS
 							q_1_out			=> GPIO_1_2,
 							q_2_out			=> GPIO_1_4,
 							q_3_out			=> GPIO_1_6,
-							q_4_out			=> GPIO_1_8,
-							q_5_out			=> GPIO_1_10,
-							q_6_out			=> GPIO_1_12,
-							q_7_out			=> GPIO_1_14,
+							-------
+							--q_4_out			=> GPIO_1_8,     -- clk
+							--q_5_out			=> GPIO_1_10,
+							--q_6_out			=> GPIO_1_12,
+							q_7_out			=> GPIO_1_14,    --zero
 						
 							q_0_in			=> GPIO_1_1,
 							q_1_in			=> GPIO_1_3,
 							q_2_in			=> GPIO_1_5,
-							q_3_in			=> GPIO_1_7,
-							q_4_in			=> GPIO_1_9,
-							q_5_in			=> GPIO_1_11,
-							q_6_in			=> GPIO_1_13,
-							q_7_in			=> GPIO_1_15
+							q_3_in			=> GPIO_1_7
+							--q_4_in			=> GPIO_1_9,
+							--q_5_in			=> GPIO_1_11,
+							--q_6_in			=> GPIO_1_13,
+							--q_7_in			=> GPIO_1_15
 		);
 	 
 
@@ -107,6 +110,7 @@ ARCHITECTURE blocks OF top_counter_verification_5 IS
     -- Concurrent Assignments  
 	 
     -- Assign outputs from intermediate signals
-
+	 GPIO_1_8 <= CLOCK_50;
+     
 end blocks;
 
